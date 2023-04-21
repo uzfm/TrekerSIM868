4d41fed147718b083ac0495468ef495165fe73e5
Yurii Cherekh <uzfm@ukr.net>
2023-04-21 22:40:37 +0300

    Change-Id: I94b1008366ecd8bb1052f980120e590ba698c835

--- USER/MesageUSB.c
+++ USER/MesageUSB.c
@@ -16,36 +16,16 @@ HAL_StatusTypeDef HAL_UART_Transmit_DMA(UART_HandleTypeDef *huart, uint8_t *pDat
 /* USER CODE BEGIN Includes */
 //--------------  UART -------
 //static unsigned char TxData[500];
-<<<<<<< HEAD
-=======
 static unsigned char RxData[300];
->>>>>>> branch 'main' of https://github.com/uzfm/TrekerSIM868.git
 //static unsigned char RxDataSize;
 
-static char           SizeGSM;
-static unsigned char RxDataGSM[256];
-
-<<<<<<< HEAD
-static char            SizeGPS;
- unsigned char RxDataGPS[300];
 
 
-
-
-static char statusDMA;
-=======
-<<<<<<< HEAD
 static char statusDMA;
 const  unsigned char ReceivSize = 50;
-=======
->>>>>>> branch 'main' of https://github.com/uzfm/TrekerSIM868.git
 
-const  unsigned char ReceivSize= 100;
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/uzfm/TrekerSIM868.git
->>>>>>> branch 'main' of https://github.com/uzfm/TrekerSIM868.git
+
 
 
 
@@ -58,11 +38,7 @@ void ReadComand(char * Bufer,int Leng){
 
 
  /// HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxData,1);
-<<<<<<< HEAD
-//	HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxData,ReceivSize);
-=======
 	HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxData,ReceivSize);
->>>>>>> branch 'main' of https://github.com/uzfm/TrekerSIM868.git
   HAL_UART_Transmit_DMA (&huart1  ,Bufer,Leng);
 
 
@@ -96,47 +72,6 @@ void Print_GSM(char*data){
 
 
 
-
-  int GPS_Read(char *data);
-void SendToUSB(void){
-
-  if((SizeGSM !=0) &&(statusDMA==0)){
-	  statusDMA=1;
-      CDC_Transmit_FS(RxDataGSM,SizeGSM);
-      SizeGSM=0;
-  }
-
-  if((SizeGPS !=0) &&(statusDMA==0)){
-	  statusDMA=1;
-      GPS_Read(RxDataGPS);
-      SizeGSM=0;
-  }
-
-
-
-
-
-  statusDMA=0;
-
-}
-
-
-void SendUSB_DT(char *Bufer, short Leng){
-	SizeGPS=0;
-	for (short var = 0;  var < Leng ;var++ ) {
-	  RxDataGSM[SizeGSM++] = Bufer[var];
-	                          } }
-
-
-
-
-void Print_GSM(char*data){
-//HAL_UART_Transmit_DMA (&huart1  ,data,  sizeof(data));
-}
-
-
-
-
 static int SizeRxData;
 
 void SendToUSB(void){
@@ -154,60 +89,21 @@ void SendToUSB(void){
 
  void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size){
       HAL_StatusTypeDef  error;
-<<<<<<< HEAD
-      unsigned char RxDataBuf[257];
-      statusDMA=1;
-
-      if(huart ==&huart1 ){
-=======
       unsigned char RxDataBuf[100];
       while (statusDMA==1);
       statusDMA=1;
                 error = HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxDataBuf,Size);
->>>>>>> branch 'main' of https://github.com/uzfm/TrekerSIM868.git
 
-          error = HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxDataBuf,Size);
-                           if( error == HAL_OK){
-                            for (short var = 0;  var < Size; ) {RxDataGSM[SizeGSM++] = RxDataBuf[var++];}
-                            error = HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxDataBuf,Size);
-                           }else{
-
-                         if( error ==HAL_BUSY){HAL_GPIO_WritePin(GPIOC, LED_WORK_Pin, GPIO_PIN_RESET);}else{
-                          if( error ==HAL_ERROR){
-                           MX_USART1_UART_Init();
-           error =  HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxDataGSM,ReceivSize);
-                          }}}
-
-
-      }else{
-                if(huart==&huart2 ){
-                error = HAL_UARTEx_ReceiveToIdle_DMA(&huart2 ,RxDataBuf,255);
                                  if( error == HAL_OK){
                                   for (short var = 0;  var < Size; ) {
-<<<<<<< HEAD
-                                        RxDataGPS[SizeGPS++] = RxDataBuf[var++];
-                                        RxDataBuf[var-1]=0; }
-                                  GPS_Read(RxDataGPS);
-                                 // CDC_Transmit_FS(RxDataGPS,SizeGPS);
-                                  SizeGSM=0;
-=======
                                 	   if(SizeRxData>=300){break;}
                                         RxData[SizeRxData++] =
                                         		RxDataBuf[var++];
                                   }
 
 
->>>>>>> branch 'main' of https://github.com/uzfm/TrekerSIM868.git
 
 
-<<<<<<< HEAD
-                                  error = HAL_UARTEx_ReceiveToIdle_DMA(&huart2 ,RxDataBuf,255);}else{
-                               if( error ==HAL_BUSY){ HAL_GPIO_WritePin(GPIOC, LED_WORK_Pin, GPIO_PIN_RESET);}else{
-                                if( error ==HAL_ERROR){
-                                 MX_USART1_UART_Init();
-                 error =  HAL_UARTEx_ReceiveToIdle_DMA(&huart2 ,RxDataGPS,255);
-                                }}}  }}
-=======
                                  }else{
 
                                if( error ==HAL_BUSY){
@@ -225,12 +121,8 @@ void SendToUSB(void){
                                 // CDC_Transmit_FS(RxData,Size);
                                 }}
 
->>>>>>> branch 'main' of https://github.com/uzfm/TrekerSIM868.git
                                  statusDMA=0;
 }
 
 
-void  ReadUart(void){
-	        HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxDataGSM, ReceivSize);
-	        HAL_UARTEx_ReceiveToIdle_DMA(&huart2 ,RxDataGPS, ReceivSize);
-}
+ HAL_StatusTypeDef  ReadUart(void){ return HAL_UARTEx_ReceiveToIdle_DMA(&huart1 ,RxData, ReceivSize);}
